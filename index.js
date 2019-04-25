22f0dffa7e2c4284fb96c55cffaa6aa42fb7000a
Daniel Heringei <daniel.heringei@incepteam.com>
2019-04-25 11:15:17 +0200

    gitignore and moved api key to config

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const WebSocket = require("ws");
 const bot = require("togethertube-vote-bot");
 const request = require('request');
-const apiKey = "";
+
 const listregex = new RegExp(/list=([A-Za-z0-9]*)/gm);
-const room = "";
+const { apiKey, room } = require('./config');
 
 bot.generatePlaySessions(1)
   .then((sessions) => {
@@ -23,7 +23,7 @@ bot.generatePlaySessions(1)
           const url = event.data.message.replace('/add ');
 
           const matches = listregex.exec(url);
-          if (matches.length >= 2) {
+          if (matches && matches.length >= 2) {
             const videoId = matches[1];
             request(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2C+id&playlistId=${videoId}&key=${apiKey}`, (error, response, body) => {
               for (const item of JSON.parse(body).items) {
@@ -45,6 +45,9 @@ bot.generatePlaySessions(1)
             });
           }
 
+        }
+        else if (event.data.message && event.data.message.startsWith('/wave')) {
+          ws.send('{"mid":"chatmessage","message":"Hello!"}');
         }
       }
     });
